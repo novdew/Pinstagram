aaa5dd41fbaf70b4f062ed652cfaec7ead6fca11
Robo Dev <89680561+robodev21@users.noreply.github.com>
2023-09-04 00:29:35 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-bt#*ti@0z$k)*4(2p$14^g8*gsz@@+%y6vt*jgchmz6$ilp^c2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://pinstagram.onrender.com']
 
