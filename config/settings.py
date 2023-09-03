c72db8f07f15662279768f0015f5f84ab6209da4
Robo Dev <89680561+robodev21@users.noreply.github.com>
2023-09-04 00:17:45 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,6 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=600)}
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -127,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR/'static/']
 MEDIA_URL = 'media/'
